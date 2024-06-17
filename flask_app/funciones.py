91d0625660c5a4a6c1ed517081b66015b60ae42a
Cesar Duque <cesar.duque01@epn.edu.ec>
2024-06-17 08:29:13 -0500

    fdsfdsf

---
+++ flask_app/funciones.py
@@ -0,0 +1,48 @@
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from nltk.stem import SnowballStemmer
+import os
+
+def cargar_corpus(directorio):
+    # Leer el contenido de los archivos en una lista
+    documentos = []
+    for archivo in os.listdir(directorio):
+        ruta_archivo = os.path.join(directorio, archivo)
+        with open(ruta_archivo, 'r', encoding='latin-1') as f:
+            documentos.append(f.read())
+
+    return documentos
+
+def cargarstopwords(ruta_stop_words):
+    with open(ruta_stop_words, 'r', encoding='latin-1') as f:
+        stop_words = set(f.read().split())
+    return stop_words
+
+def crearbow(documentos,stop_words):
+    documentos_limpios = [limpiar_texto(doc) for doc in documentos]
+    documentos_tokenizados_split = [separar(doc) for doc in documentos_limpios]
+    documentos_procesados = [procesar_tokens(doc,stop_words) for doc in documentos_tokenizados_split]
+    documentos_procesados_texto = [' '.join(doc) for doc in documentos_procesados]
+    bow = CountVectorizer().fit_transform(documentos_procesados_texto)
+    return bow
+
+def procesar_tokens(tokens,stop_words):
+    stemmer = SnowballStemmer('english')
+    # Eliminar stop words
+    tokens_filtrados = [token for token in tokens if token not in stop_words]
+    # Aplicar stemming
+    tokens_stemmizados = [stemmer.stem(token) for token in tokens_filtrados]
+    return tokens_stemmizados
+
+def separar(doc):
+    palabras = doc.split()
+    return palabras
+def limpiar_texto(texto):
+    # Eliminar caracteres no deseados (mantener solo letras y espacios)
+    texto_limpio = re.sub(r'[^a-zA-Z\s]', '', texto)
+    # Normalizar a minúsculas
+    texto_limpio = texto_limpio.lower()
+    # Eliminar espacios en blanco adicionales
+    texto_limpio = re.sub(r'\s+', ' ', texto_limpio).strip()
+    return  texto_limpio
---
+++ flask_app/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import funciones as fun
+
+app = Flask(__name__)
+
+
+documentos=fun.cargar_corpus('../reuters/training/')
+print(f'Se han cargado {len(documentos)} documentos.')
+stop_words=fun.cargarstopwords('../reuters/stopwords')
+print(f'Se han cargado {len(stop_words)} stopwords.')
+bow=fun.crearbow(documentos,stop_words)
+print(f'Se ha creado una matriz BOW de tamaño {bow.shape}.')
+
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+# @app.route('/buscar', methods=['POST'])
+# def buscar():
+#     consulta = request.form['consulta']
+#     resultados = fun.procesar_y_buscar(consulta)
+#     return render_template('resultados.html', consulta=consulta, resultados=resultados)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5005)
+    
