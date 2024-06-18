d5789f046669b670e5301ae1670c246d4b0e20e5
Cesar Duque <cesar.duque01@epn.edu.ec>
2024-06-17 23:13:13 -0500

    fdsfds

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import funciones as fun
 
 app = Flask(__name__)
 
-
+vectorizer_bow = CountVectorizer()
+vectorizer_tfidf = TfidfVectorizer()
 documentos=fun.cargar_corpus('../reuters/training/')
 print(f'Se han cargado {len(documentos)} documentos.')
 stop_words=fun.cargarstopwords('../reuters/stopwords')
 print(f'Se han cargado {len(stop_words)} stopwords.')
-bow=fun.crearbow(documentos,stop_words)
+bow,vectorizer_bow=fun.crearbow(documentos,stop_words)
+
 print(f'Se ha creado una matriz BOW de tamaño {bow.shape}.')
+
 
 
 @app.route('/')
@@ -22,10 +26,16 @@ def buscar():
     consulta = request.form['consulta']
     radio= request.form['method']
     if radio=='bow':
-        data={ 'metodo':'bow',
-              'consulta':consulta}
+        respuesta=fun.buscar_bow(consulta,fun.construir_indice_invertido(documentos),bow,vectorizer_bow,stop_words)
+        resultados = []
+        for doc_id,similitud_cos in respuesta:
+            lineas = documentos[doc_id].split('\n')  # Dividir el documento en líneas
+            titulo = lineas[0]  # La primera línea es el título
+            contenido = " ".join(lineas)  # Unir todas las líneas para el contenido completo
+            similitud_coseno=similitud_cos
+            resultados.append({"titulo": titulo, "contenido": contenido,"similitud_coseno":similitud_coseno})
         
-        return render_template('resultados.html', data=data)
+        return render_template('resultados.html', resultados=resultados)
     
 
 
